a9ababc299f24a28c82ecb8b9aed99d8f4befbfd
Aleksandr Glebik <glebikaa@gmail.com>
2024-06-19 23:20:06 +0300

    feat: add hook useGameState; refactor GameField

--- components/GameField/GameField.tsx
+++ components/GameField/GameField.tsx
@@ -1,42 +1,21 @@
 'use client';
 
-import { useCallback, useState } from 'react';
-
 import GameInfo from '../GameInfo/GameInfo';
 import GameCell from '../GameCell/GameCell';
 
 import s from './GameField.module.scss';
-import { SYMBOL_O, SYMBOL_X, INITIAL_CELLS } from '@/lib/constants/constants';
-import { computeWinner } from '@/utils/func/computeWinner';
+import { useGameState } from '@/utils/hooks/useGameState';
 
 export default function GameField() {
-  const [currentSymbol, setCurrentSymbol] = useState<string | null>(SYMBOL_O);
-  const [cells, setCells] = useState(INITIAL_CELLS);
-  const [winSequence, setWinSequence] = useState<number[] | undefined>();
-
-  const winnerSymbol = winSequence ? cells[winSequence?.[0]] : currentSymbol;
-  const isDraw = !winSequence && cells.filter((value) => value).length === 9;
-
-  const handleCellClick = (ind: number) => {
-    if (cells[ind] || winSequence) {
-      return;
-    }
-
-    const copyCells = cells.slice();
-    copyCells[ind] = currentSymbol;
-
-    const winner = computeWinner(copyCells);
-
-    setCells(copyCells);
-    setCurrentSymbol(currentSymbol === SYMBOL_X ? SYMBOL_O : SYMBOL_X);
-    setWinSequence(winner);
-  };
-
-  const handleResetClick = useCallback(() => {
-    setCells(INITIAL_CELLS);
-    setWinSequence(undefined);
-    setCurrentSymbol(SYMBOL_O);
-  }, []);
+  const {
+    isDraw,
+    winSequence,
+    winnerSymbol,
+    currentSymbol,
+    cells,
+    handleCellClick,
+    handleResetClick,
+  } = useGameState();
 
   return (
     <div className={s.Field}>
@@ -48,7 +27,7 @@ export default function GameField() {
       />
       <div className={s.GameCells}>
         {cells.map((el, index) => (
-          <GameCell  
+          <GameCell
             key={`${el}_${index}`}
             element={el}
             onClick={() => handleCellClick(index)}
---
+++ utils/hooks/useGameState.ts
@@ -0,0 +1,44 @@
+import { useState, useCallback } from 'react';
+
+import { INITIAL_CELLS, SYMBOL_O, SYMBOL_X } from '@/lib/constants/constants';
+import { computeWinner } from '../func/computeWinner';
+
+export const useGameState = () => {
+  const [currentSymbol, setCurrentSymbol] = useState<string | null>(SYMBOL_O);
+  const [cells, setCells] = useState(INITIAL_CELLS);
+  const [winSequence, setWinSequence] = useState<number[] | undefined>();
+
+  const winnerSymbol = winSequence ? cells[winSequence?.[0]] : currentSymbol;
+  const isDraw = !winSequence && cells.filter((value) => value).length === 9;
+
+  const handleCellClick = (ind: number) => {
+    if (cells[ind] || winSequence) {
+      return;
+    }
+
+    const copyCells = cells.slice();
+    copyCells[ind] = currentSymbol;
+
+    const winner = computeWinner(copyCells);
+
+    setCells(copyCells);
+    setCurrentSymbol(currentSymbol === SYMBOL_X ? SYMBOL_O : SYMBOL_X);
+    setWinSequence(winner);
+  };
+
+  const handleResetClick = useCallback(() => {
+    setCells(INITIAL_CELLS);
+    setWinSequence(undefined);
+    setCurrentSymbol(SYMBOL_O);
+  }, []);
+
+  return {
+    currentSymbol,
+    cells,
+    winSequence,
+    winnerSymbol,
+    isDraw,
+    handleCellClick,
+    handleResetClick,
+  };
+};
