ae191eaf531d68187a7cf86d3f00c11c4100a6a2
Aleksandr Glebik <glebikaa@gmail.com>
2024-06-19 12:28:46 +0300

    init: start project, i sass, clsx

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./globals.scss";
+
+const OpenSans = Open_Sans({ subsets: ["latin"], weight: ["400", "600"] });
+
+export const metadata: Metadata = {
+  title: "Naughts and crosses",
+  description: "Game Naughts and crosses (React, Next, TypeScript, SCSS)",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={OpenSans.className}>{children}</body>
+    </html>
+  );
+}
---
+++ components/Symbol/Symbol.tsx
@@ -0,0 +1,9 @@
+import styles from "./Symbol.module.scss";
+
+export default function Symbol() {
+  return (
+    <main className={styles.main}>
+      init project
+    </main>
+  );
+}
